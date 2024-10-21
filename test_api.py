0994bfea41540f5148d6a69041c315fa5309f4a7
BalakumarSathaiah <balakumar15698.com>
2024-10-21 09:57:23 +0800

    Updated test_api.py with new changes

--- test_api.py
+++ test_api.py
@@ -1,4 +1,3 @@
-#test_api.py
 import requests
 
 # Function to load PDF and classify
@@ -11,20 +10,28 @@ def test_pdf_classification():
         print(f"Testing with: {pdf_file_path}")
         with open(pdf_file_path, 'rb') as f:
             load_response = requests.post("http://127.0.0.1:8000/load_pdf", files={"file": f})
-            print("Load PDF Response:", load_response.json())
+            print("Load PDF Response:")
+            print(f"Filename: {load_response.json().get('filename')}")
+            print(f"Extracted Text:\n{load_response.json().get('extracted_text')}\n")
+            print(f"Labels: {load_response.json().get('labels')}")
+            print(f"Scores: {load_response.json().get('scores')}\n")
 
         extracted_text = load_response.json().get("extracted_text")
         classification_response = requests.post(
             "http://127.0.0.1:8000/classify", 
             json={"text": extracted_text}
         )
-        print("Classification Response:", classification_response.json())
+        print("Classification Response:")
+        print(classification_response.json(), "\n")
 
         entities_response = requests.post(
             "http://127.0.0.1:8000/extract_entities", 
             json={"text": extracted_text}
         )
-        print("Extract Entities Response:", entities_response.json())
+        print("Extract Entities Response:")
+        for entity in entities_response.json().get("entities"):
+            print(f"Entity: {entity[0]}, Label: {entity[1]}")
+        print("\n" + "="*50 + "\n")
 
 if __name__ == "__main__":
     test_pdf_classification()
